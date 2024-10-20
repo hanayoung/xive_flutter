fd26ad1b6e6f1399fd72cbb67302cf28766b0238
yeiin <yeini0417@gmail.com>
2024-10-20 12:22:54 +0900

    audio stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:xive/routes/pages.dart';
+
+final lightModeTheme = ThemeData(
+  fontFamily: "Pretendard",
+  colorScheme: const ColorScheme.light( surface: Colors.white,),
+  primaryColor: const Color(0xff8000FF),
+  disabledColor: const Color(0xFFD9D9D9),
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.black,
+      fontSize: 16,
+      letterSpacing: -0.02,
+    ),
+    bodySmall: TextStyle(
+      color: Color(0xFF191919),
+      fontSize: 14,
+      letterSpacing: -0.02,
+    ),
+    titleMedium: TextStyle(color: Colors.black, fontSize: 24, letterSpacing: -0.02, fontWeight: FontWeight.w700,),
+    titleSmall: TextStyle(color: Colors.black, fontSize: 16, letterSpacing: -0.02, fontWeight: FontWeight.w600,),),
+  dividerColor: const Color(0xFFF4F4F4),
+);
+
+final darkModeTheme = ThemeData(
+  fontFamily: "Pretendard",
+  colorScheme: const ColorScheme.light( surface: Colors.white,),
+  primaryColor: const Color(0xff8000FF),
+  disabledColor: const Color(0xFFD9D9D9),
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+      letterSpacing: -0.02,
+    ),
+    bodySmall: TextStyle(
+      color: Color(0xFF191919),
+      fontSize: 14,
+      letterSpacing: -0.02,
+    ),
+    titleMedium: TextStyle(color: Colors.white, fontSize: 24, letterSpacing: -0.02, fontWeight: FontWeight.w700,),
+    titleSmall: TextStyle(color: Colors.white, fontSize: 16, letterSpacing: -0.02, fontWeight: FontWeight.w600,),),
+  dividerColor: const Color(0xFFF4F4F4),
+);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,39 +67,9 @@ class MyApp extends StatelessWidget {
       title: 'XIVE',
       locale: const Locale('ko'),
       getPages: Pages.pages,
-      theme: ThemeData(
-        fontFamily: "Pretendard",
-        colorScheme: const ColorScheme.light(
-          surface: Colors.white,
-        ),
-        primaryColor: const Color(0xff8000FF),
-        disabledColor: const Color(0xFFD9D9D9),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            letterSpacing: -0.02,
-          ),
-          bodySmall: TextStyle(
-            color: Color(0xFF191919),
-            fontSize: 14,
-            letterSpacing: -0.02,
-          ),
-          titleMedium: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            letterSpacing: -0.02,
-            fontWeight: FontWeight.w700,
-          ),
-          titleSmall: TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            letterSpacing: -0.02,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        dividerColor: const Color(0xFFF4F4F4),
-      ),
+      theme: lightModeTheme,
+      darkTheme: darkModeTheme,
+      themeMode: ThemeMode.system,
     );
   }
 }
--- lib/screens/audio_screen.dart
+++ lib/screens/audio_screen.dart
@@ -1,75 +1,22 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 import 'package:xive/controllers/audio_controller.dart';
 import 'package:xive/controllers/audio_play_controller.dart';
+import 'package:xive/widgets/audio/audio_card.dart';
 
-const appBarTextStyle = TextStyle(color: Colors.white, fontSize: 16, fontFamily: 'Pretendard', fontWeight: FontWeight.w400, letterSpacing: -0.32,);
+import '../widgets/audio/audio_header.dart';
+
+final appBarTextStyle = TextStyle(color: Get.isDarkMode?Colors.white:Colors.black, fontSize: 16, fontFamily: 'Pretendard', fontWeight: FontWeight.w400, letterSpacing: -0.32,);
 
 class AudioScreen extends StatelessWidget {
   const AudioScreen({super.key});
 
-  Widget playCard(int index){
-    return Obx(() => Container(
-      padding: EdgeInsets.symmetric(vertical: 8),
-      child: InkWell(
-        onTap: (){
-          AudioController.to.changePlayIndex(index);
-        },
-        child: AudioController.to.isPlayBar.value && AudioController.to.playIndex == index?
-        Row(
-          children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: AudioController.to.playList[index]["audioImageUrl"] != null ?
-                  Image.network(AudioController.to.playList[index]["audioImageUrl"], height: 56, width: 56, fit: BoxFit.fill,)
-                      :Image.asset("assets/images/audio_example.png", height: 56, width: 56, fit: BoxFit.fill,),
-                ),
-                Container(
-                  color: Colors.black.withOpacity(0.6),
-                  width: 56,
-                  height: 56,
-                  child: Center(child: Image.asset("assets/images/play_vector.png")),
-                ),
-
-              ],
-            ),
-            const SizedBox(width: 8,),
-            Expanded(
-              child:Text(
-                '${index+1}.  ${AudioController.to.playList[index]["audioName"]}',
-                style: const TextStyle(color: Colors.white, fontSize: 16, fontFamily: 'Pretendard', fontWeight: FontWeight.w600, letterSpacing: -0.32,),
-              ),
-            )
-          ],
-        ): Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: AudioController.to.playList[index]["audioImageUrl"] != null ?
-              Image.network(AudioController.to.playList[index]["audioImageUrl"], height: 56, width: 56, fit: BoxFit.fill,)
-                  :Image.asset("assets/images/audio_example.png", height: 56, width: 56, fit: BoxFit.fill,),
-            ),
-            const SizedBox(width: 8,),
-            Expanded(
-              child:Text(
-              '${index+1}.  ${AudioController.to.playList[index]["audioName"]}',
-              style: const TextStyle(color: Color(0xFFDFDFDF), fontSize: 16, fontFamily: 'Pretendard', fontWeight: FontWeight.w400, letterSpacing: -0.32,),
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
-  }
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
+
     AudioController.to.buildContext = context;
     AudioPlayController.to.buildContext = context;
 
@@ -77,35 +24,43 @@ class AudioScreen extends StatelessWidget {
       return Scaffold(
         body: Stack(
           children: [
-            c.audioHeaderContents != null && c.audioHeaderContents["eventBackgroundImageUrl"] != null?
-            Image.network(c.audioHeaderContents["eventBackgroundImageUrl"].toString(), height: double.infinity, fit: BoxFit.fitHeight,):
-            Image.asset("assets/images/audio_example.png", height: double.infinity, fit: BoxFit.fitHeight,),
+             c.audioHeaderContents["eventBackgroundImageUrl"] != ""?
+            Image.network(
+              c.audioHeaderContents["eventBackgroundImageUrl"].toString(),
+              height: double.infinity,
+              fit: BoxFit.fitHeight,
+            ): Get.isDarkMode?Container(color: Colors.black):Container(color: Colors.white,),
             Scaffold(
-              backgroundColor: Color(0xCC000000),
+              backgroundColor: Get.isDarkMode?Colors.black.withOpacity(0.8):Colors.white.withOpacity(0.8),
               appBar: AppBar(
-                  backgroundColor: Colors.transparent, iconTheme: const IconThemeData(color: Colors.white,),
-                  title: const Text("오디오 가이드",style: appBarTextStyle),
+                  backgroundColor: Colors.transparent, iconTheme: IconThemeData(color: Get.isDarkMode?Colors.white:Colors.black),
+                  title: Text("오디오 가이드",style: appBarTextStyle),
+                leading: IconButton(onPressed: (){
+                  AudioPlayController.to.endPlayer();
+                  Get.back();
+                }, icon: const Icon(Icons.chevron_left)),
                 ),
                 body: SafeArea(
                   child: Container(
                     padding: const EdgeInsets.symmetric(horizontal: 24),
                     child: Column(
                       children: [
+                        AudioHeader(),
                         Expanded(
                           child: SingleChildScrollView(
-                            physics: NeverScrollableScrollPhysics(),
                             child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.stretch,
-                                mainAxisSize: MainAxisSize.min,
+                                mainAxisSize: MainAxisSize.max,
                                 children: [
-                                  c.audioHeader(),
-                                  ListView(
-                                    shrinkWrap: true,
-                                    children: [
-                                      for(int i=0; i<c.playList.length; i++)
-                                        playCard(i),
-                                    ],
-                                  ),
+                                  ListView.builder(
+                                      shrinkWrap: true,
+                                      physics: NeverScrollableScrollPhysics(),
+                                      itemCount: c.playList.length,
+                                      itemBuilder: (context,index){
+                                        return AudioCard(index: index);
+                                      }
+                                  )
+
                                 ],
                               ),
                           ),
